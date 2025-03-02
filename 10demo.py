83e0fd072dce25b591e4f32c1278363032299de3
dabdelnaby <abdelnabydean@gmail.com>
2025-03-02 09:24:37 -0800

    up

--- 10demo.py
+++ 10demo.py
@@ -8,4 +8,19 @@ this is a
  multi-line comment
 """
 
-print(10**2)
+import math #importing math module
+a = 3 # side 1
+b = 4 # side 2
+c = math.sqrt(a**+2 + b**2) # pythag theorem
+print(c) 
+
+print(type(a), type(b), type(c), sep=',', end='!\n')
+
+## functions
+
+def pythagoras(a,b):
+    return math.sqrt(a**2 + b**2)
+    
+
+hyp = pythagoras(3, 4)
+print(hyp)
